a6799a2bd6fc22f79818faa243302e2b95e6be22
Thais Nardiellen <nardiellensouza@gmail.com>
2019-06-04 01:03:50 -0300

    Criar classes 2

---
+++ src/Quest3/Administrador.java
@@ -0,0 +1,21 @@
+package Quest3;
+
+public class Administrador extends Empregado{
+    
+    private double ajudadeCusto;
+    
+    public Administrador(String nome, String end, int tel, int s, double x, double i, double a){
+        super(nome, end, tel, s, x, i);
+        setAjudaCusto(a);
+        
+    }
+    public double getAjudaCusto(){
+        return this.ajudadeCusto;
+    }
+    public void setAjudaCusto(double a){
+        this.ajudadeCusto=a;
+    }
+    public double calcularSalario(){
+        return this.getAjudaCusto()+super.calcularSalario();
+    }
+}
